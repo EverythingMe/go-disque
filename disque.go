6c66b33816702f3f71c17e910b1fc60f2e540191
Dvir Volk <dvirsky@gmail.com>
2015-04-29 00:19:23 +0300

    initial commit

---
+++ disque.go
@@ -0,0 +1,257 @@
+// Package disque implements a simlpe client for the Disque in-memory distributed queue [https://github.com/antirez/disque]
+package disque
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+// Job describes a job that is about to be enqueued or is received from the client for proecessing
+type Job struct {
+	// The name of the queue this job is sent/received to/from
+	Queue string
+	// Job data - this can be anything
+	Data []byte
+
+	// the job id, available only for jobs received from the client, not enqueued
+	id string
+}
+
+// Id returns the job id. the underlying id is only settable by the client, so there is only a getter for it
+func (j Job) Id() string {
+	return j.id
+}
+
+// AddRequest describes how you want a job to be enqueued
+type AddRequest struct {
+	// The job about to be added to the queue
+	Job Job
+	// If the add is not async, this is the maximal timeout for replicating the job to the minimal number of nodes
+	Timeout time.Duration
+	//  the number of nodes the job should be replicated to.
+	Replicate int
+	//  the duration that should elapse before the job is queued by any server
+	Delay time.Duration
+	// A duration after which, if no ACK is received, the job is put again into the queue for delivery
+	Retry time.Duration
+	// the max job life in seconds. After this time, the job is deleted even if it was not successfully delivered
+	TTL time.Duration
+	// if there are already Maxlen messages queued for the specified queue, adding returns an error
+	Maxlen int
+	// If set to true, the add command returns ASAP and replicates the job to other nodes in the background
+	Async bool
+}
+
+// Node describes a node in the cluster, received from Hello
+type Node struct {
+	Id       string
+	Addr     string
+	Priority int
+}
+
+// HelloResponse is returned from the Hello command and tells us the state of the cluster
+type HelloResponse struct {
+	versionId string
+	NodeId    string
+	Nodes     []Node
+}
+
+// Client is the interface that describes a disque client
+type Client interface {
+
+	// Add sents an ADDJOB command to disque, as specified by the AddRequest. Returns the job id or an error
+	Add(AddRequest) (string, error)
+
+	// Get gets one job from any of the given queues, or times out if timeout has elapsed without a job being available. Returns a job or an error
+	Get(timeout time.Duration, queues ...string) (Job, error)
+
+	// GetMulti gets <count> jobs from the given queues, or times out if timeout has elapsed without enough jobs being available. Returns a job or an error
+	GetMulti(count int, timeout time.Duration, queues ...string) ([]Job, error)
+
+	// Ack sends and ACKJOB command with the given job ids
+	Ack(jobIds ...string) error
+
+	// FastAck sends a FASTACK commadn with the given job ids. See the disque docs about the
+	// difference between ACK and FASTACK
+	FastAck(jobIds ...string) error
+
+	// Qlen returns the length of a given queue
+	Qlen(qname string) (int, error)
+
+	// Hello is a handshake request with the server, returns a description of the cluster state
+	Hello() (HelloResponse, error)
+
+	// Close closes the underlying connection
+	Close() error
+}
+
+// RedisClient implements a redigo based client
+type RedisClient struct {
+	conn  redis.Conn
+	net   string
+	addrs []string
+}
+
+// Dial tries to connect to one of the addrs of the servers, and returns a new client if it succeeds
+func Dial(net string, timeout time.Duration, addrs ...string) (client Client, err error) {
+
+	var conn redis.Conn
+	for _, addr := range addrs {
+
+		if conn, err = redis.DialTimeout(net, addr, timeout, timeout, timeout); err != nil {
+			continue
+		}
+
+		client = &RedisClient{
+			conn:  conn,
+			net:   net,
+			addrs: addrs,
+		}
+
+		break
+
+	}
+
+	return
+}
+
+// Close closes the underlying connection
+func (c *RedisClient) Close() error {
+	return c.conn.Close()
+}
+
+// Add sents an ADDJOB command to disque, as specified by the AddRequest. Returns the job id or an error
+func (c *RedisClient) Add(r AddRequest) (string, error) {
+	//ADDJOB queue_name job <ms-timeout> [REPLICATE <count>] [DELAY <sec>] [RETRY <sec>] [TTL <sec>] [MAXLEN <count>] [ASYNC]
+
+	args := redis.Args{r.Job.Queue, r.Job.Data, int(r.Timeout / time.Millisecond)}
+
+	if r.Replicate > 0 {
+		args = args.Add("REPLICATE", r.Replicate)
+	}
+
+	if r.Delay > 0 {
+		args = args.Add("DELAY", int64(r.Delay.Seconds()))
+	}
+
+	if r.Retry > 0 {
+		args = args.Add("DELAY", int64(r.Retry.Seconds()))
+	}
+
+	if r.TTL > 0 {
+		args = args.Add("DELAY", int64(r.TTL.Seconds()))
+	}
+
+	if r.Maxlen > 0 {
+		args = args.Add("MAXLEN", r.Maxlen)
+	}
+
+	if r.Async {
+		args = args.Add("ASYNC")
+	}
+	fmt.Println(args)
+	id, err := redis.String(c.conn.Do("ADDJOB", args...))
+	if err != nil {
+		return "", errors.New("disque: could not add job: " + err.Error())
+	}
+	return id, nil
+}
+
+// Get gets one job from any of the given queues, or times out if timeout has elapsed without a job being available.
+// Returns a job or an error
+func (c *RedisClient) Get(timeout time.Duration, queues ...string) (Job, error) {
+
+	ret, err := c.GetMulti(0, timeout, queues...)
+	if err != nil {
+		return Job{}, err
+	}
+	if ret == nil || len(ret) == 0 {
+		return Job{}, errors.New("disque: no jobs returned")
+	}
+	return ret[0], nil
+
+}
+
+// GetMulti gets <count> jobs from the given queues, or times out if timeout has elapsed without
+// enough jobs being available. Returns a list of jobs or an error
+func (c *RedisClient) GetMulti(count int, timeout time.Duration, queues ...string) ([]Job, error) {
+
+	if len(queues) == 0 {
+		return nil, errors.New("disque: no queues specified")
+	}
+	if count < 0 {
+		return nil, fmt.Errorf("disque: invalid count %d", count)
+	}
+
+	args := redis.Args{}
+	if timeout > 0 {
+		args.Add("TIMEOUT", timeout/time.Millisecond)
+	}
+	if count > 0 {
+		args = args.Add("COUNT", count)
+	}
+	args = args.Add("FROM")
+	args = args.AddFlat(queues)
+
+	vals, err := redis.Values(c.conn.Do("GETJOB", args...))
+
+	if err != nil {
+		return nil, fmt.Errorf("disque: could not get jobs: %s", err)
+	}
+
+	ret := make([]Job, 0, len(vals))
+
+	for _, v := range vals {
+		if arr, ok := v.([]interface{}); ok {
+
+			ret = append(ret, Job{
+				Queue: string(arr[0].([]byte)),
+				id:    string(arr[1].([]byte)),
+				Data:  arr[2].([]byte),
+			})
+
+		}
+
+	}
+
+	return ret, nil
+
+}
+
+// Ack sends and ACKJOB command with the given job ids
+func (c *RedisClient) Ack(jobIds ...string) error {
+
+	args := make(redis.Args, 0, len(jobIds))
+	args.AddFlat(jobIds)
+	if _, err := c.conn.Do("ACKJOB", args...); err != nil {
+		return fmt.Errorf("disque: error sending ACK: %s", err)
+	}
+	return nil
+}
+
+// FastAck sends a FASTACK commadn with the given job ids. See the disque docs about the
+// difference between ACK and FASTACK
+func (c *RedisClient) FastAck(jobIds ...string) error {
+	args := make(redis.Args, 0, len(jobIds))
+	args.AddFlat(jobIds)
+	if _, err := c.conn.Do("FASTACK", args...); err != nil {
+		return fmt.Errorf("disque: error sending ACK: %s", err)
+	}
+	return nil
+}
+
+// Qlen returns the length of a given queue
+func (c *RedisClient) Qlen(qname string) (int, error) {
+
+	return redis.Int(c.conn.Do("QLEN", qname))
+
+}
+
+// Hello is a handshake request with the server, returns a description of the cluster state
+// TODO: implement this
+func (c *RedisClient) Hello() (HelloResponse, error) {
+	return HelloResponse{}, errors.New("Hello not implemented yet")
+}
---
+++ disque_test.go
@@ -0,0 +1,92 @@
+package disque
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+const addr = "localhost:7711"
+
+func TestAddJob(t *testing.T) {
+
+	client, err := Dial("tcp", time.Second, addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	ja := AddRequest{
+		Job: Job{
+			Queue: "test",
+			Data:  []byte("foo"),
+		},
+		Timeout: time.Millisecond * 100,
+	}
+
+	id, err := client.Add(ja)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if id == "" {
+		t.Errorf("Invalid id")
+	}
+
+	fmt.Println("id: ", id)
+	//TODO: Add more edge case tests
+}
+
+func TestClient(t *testing.T) {
+	client, err := Dial("tcp", time.Second, addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+	qname := "test1"
+	ja := AddRequest{
+		Job: Job{
+			Queue: qname,
+			Data:  []byte("foo"),
+		},
+		Timeout: time.Millisecond * 100,
+	}
+
+	id, err := client.Add(ja)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if l, err := client.Qlen(qname); err != nil {
+		t.Error(err)
+	} else if l != 1 {
+		t.Errorf("Wrong queue len. expected 1, got %d", l)
+	}
+
+	job, err := client.Get(time.Second, qname)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if l, err := client.Qlen(qname); err != nil {
+		t.Error(err)
+	} else if l != 0 {
+		t.Errorf("Wrong queue len. expected 0, got %d", l)
+	}
+
+	if job.Id() != id {
+		t.Errorf("Expected id %s, got %s", id, job.Id())
+	}
+
+	if string(job.Data) != string(ja.Job.Data) {
+		t.Errorf("Wrong job data. got %s, expected %s", job.Data, ja.Job.Data)
+	}
+
+	if job.Queue != ja.Job.Queue {
+		t.Errorf("Got wrong queue name, expected %s, got %s", ja.Job.Queue, job.Queue)
+	}
+	if err = client.Ack(job.Id()); err != nil {
+		t.Errorf("Error ACKing: %s", err)
+	}
+
+}
